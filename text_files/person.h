6c4b6248597e08411dd4a6f3797e340a742b3790
Olga Tratsiuk <lelikiris@gmail.com>
2020-10-20 23:36:02 +0300

    Fix header file

--- text_files/person.h
+++ text_files/person.h
@@ -1,5 +1,7 @@
 #ifndef _PERSON_H_
 #define _PERSON_H_
+
+#include <string>
 #include <vector>
 
 class Person {
@@ -19,7 +21,25 @@ public:
   std::vector<data> array;
 
 public:
-  void load_from_file(const std::string &file_name)
-}
+  void load_from_file(const std::string &file_name);
+
+  void load_to_file(const std::string &file_name);
+
+  void add_person();
+
+  void display_person(data &person);
+
+  void display_array();
+
+  void sort_by_surname();
+
+  void sort_by_age();
+
+  void display_by_month(int month);
+
+  void the_oldest_man();
+
+  void display_by_surname(char first_letter);
+};
 
 #endif
