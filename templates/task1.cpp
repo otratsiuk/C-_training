039113712b9efe5a893a47f157623a84b11abb01
Olga Tratsiuk <lelikiris@gmail.com>
2020-11-11 01:40:03 +0300

    Change functions names

--- templates/task1.cpp
+++ templates/task1.cpp
@@ -2,34 +2,35 @@
 #include <iostream>
 #include <string>
 
-std::string find_max(std::string &a, std::string &b) {
+std::string find_max_string(std::string &a, std::string &b) {
   return a.length() > b.length() ? a : b;
 }
 
-template <typename T> T find_max(T a, T b) { return a > b ? a : b; }
+template <typename T> T find_max_in_pair(T a, T b) { return a > b ? a : b; }
 
-template <typename T, int length> T find_max(const T (&array)[length]) {
+template <typename T, int length>
+T find_max_in_array(const T (&array)[length]) {
   T max = array[0];
 
   for (int i = 1; i < length; i++) {
-    max = find_max(max, array[i]);
+    max = find_max_in_pair(max, array[i]);
   }
   return max;
 }
 
 int main() {
   std::cout << std::setw(20) << std::left
-            << "max number: " << find_max(-3.4, 10.0) << "\n";
+            << "max number: " << find_max_in_pair(-3.4, 10.0) << "\n";
 
   int array[7] = {3, -4, 9, -23, 182, -4, 1};
 
   std::cout << std::setw(20) << std::left
-            << "max array number: " << find_max(array) << "\n";
+            << "max array number: " << find_max_in_array(array) << "\n";
 
   std::string a("Cat");
   std::string b("CatDog");
   std::cout << std::setw(20) << std::left
-            << "the longest string: " << find_max(a, b) << "\n";
+            << "the longest string: " << find_max_string(a, b) << "\n";
 
   return 0;
 }
